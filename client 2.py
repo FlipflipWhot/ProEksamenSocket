26f81ce929892870cc4465100ec77c77941f6da8
FlipflipWhot <123778128+FlipflipWhot@users.noreply.github.com>
2023-05-11 21:46:10 +0200

    Update client 2.py

--- client 2.py
+++ client 2.py
@@ -7,6 +7,7 @@ width = 500
 height = 500
 win = pg.display.set_mode((width, height))
 pg.display.set_caption("Client")
+pg.init()
 
 
 def redrawWindow(win, player, player2):
@@ -36,4 +37,35 @@ def main():
         p.move()
         redrawWindow(win, p, p2)
 
-main()
+def menu_screen():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        win.fill((128, 128, 128))
+        font = pygame.font.SysFont("comicsans", 60)
+        text = font.render(f"Click to Play!", 1, (255,0,0))
+        font = pygame.font.SysFont("comicsans", 15)
+        txt = font.render(f"You can change your to blue with b, red with r and yellow with y", 1, (255, 0, 0))
+        win.blit(text, (100,200))
+        win.blit(txt, (50, 300))
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            keys = pg.key.get_pressed()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                run = False
+
+
+
+
+
+    main()
+
+while True:
+    menu_screen()
